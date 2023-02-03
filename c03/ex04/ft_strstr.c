93de8ce4d7d226b0f5c9152140bd83927817192b
tienbui1508 <tienbui1508@gmail.com>
2023-02-04 01:26:02 +1030

    c03 up up ex04. todo: norminette

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -4,7 +4,7 @@ char	*ft_strstr(char *str, char *to_find)
 	int	i;
 	int	j;
 	int	len;
-	bool found;
+	bool	found;
 
 	i = 0;
 	found = false;
@@ -13,14 +13,24 @@ char	*ft_strstr(char *str, char *to_find)
 	while (str[i] != '\0')
 	{
 		j = 0;
-		while (str[i + j] == to_find[j] )
+		while (str[i + j] == to_find[j])
 		{
 			if (to_find[j + 1] == '\0')
 				return (&str[i]);
 			j++;
 		}
 		i++;
-
-
 	}
+	return (0);
+}
+
+//test ex04
+#include <stdio.h>
+#include <string.h>
+int	main()
+{
+	char str[] = "42 Adelaide week 2 piscine";
+	char find[] = "week";
+	printf("%s\n", strstr(str, find));
+	printf("%s\n", ft_strstr(str, find));
 }
